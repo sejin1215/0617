f4791799b7f9f881939e6f54a6b5ed84a315d88e
sejin1215 <sdh210419@sdh.hs.kr>
2021-06-17 19:53:52 +0900

    Express

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 let express = require('express');
 let app = express();
 
+app.set(`view engine`, `ejs`);
+
+app.use(express.json())
+
+app.use(express.urlencoded())
+
 app.get('/', (req, res) => {
-    res.send(`Hello World!`);
+    res.render(`index`);
+});
+
+app.get('/test', (req, res) => {
+    res.render(`test`);
+});
+
+app.post(`/submit`, (req, res) => {
+    console.log(req.body);
+    res.redirect(`/`);              // / 경로로 리다이렉트
 });
 
 app.listen(3000, () => {
